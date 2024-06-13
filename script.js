725a7ba3c52b962611f9c66c4fd6723c787cd259
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2024-06-13 12:32:48 -0600

    Adding page navigation

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const btnScrollToSection1 = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +22,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -27,5 +30,15 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// Page navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const elementId = e.target.getAttribute('href');
+    document.querySelector(elementId).scrollIntoView({ behavior: 'smooth' });
   }
 });
